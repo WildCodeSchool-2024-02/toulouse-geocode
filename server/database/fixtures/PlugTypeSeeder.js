88c3adfad77135d0dec7ecca945277bde54ca338
Modrum <tristanlaroye@hotmail.com>
2024-06-13 11:53:50 +0200

    feat: add schema, seeders and csv's datas

---
+++ server/database/fixtures/PlugTypeSeeder.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const fs = require("fs");
+const AbstractSeeder = require("./AbstractSeeder");
+
+class PlugTypeSeeder extends AbstractSeeder {
+  constructor() {
+    super({ table: "plug_type", truncate: true });
+    const src = path.join(__dirname, "..", "sources", "plug_type.csv");
+    this.content = fs.readFileSync(src, "utf8");
+  }
+
+  run() {
+    const rows = this.content.split("\n");
+    rows.forEach((row) => {
+      const cols = row.split(";");
+      const [plugTypeEf, plugType2, plugTypeComboCcs, plugTypeChadeLu, plugTypeAutre] = cols;
+      const obj = {
+        plug_type_ef: plugTypeEf,
+        plug_type_2: plugType2,
+        plug_type_combo_ccs: plugTypeComboCcs,
+        plug_type_chadeLu: plugTypeChadeLu,
+        plug_type_autre: plugTypeAutre,
+      };
+      this.insert(obj);
+    });
+  }
+}
+
+module.exports = PlugTypeSeeder;
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,39 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE TABLE charging_station(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+id_station_itinerance VARCHAR(255) NULL,
+station_name VARCHAR(255) NULL,
+station_implantation VARCHAR(255) NULL,
+station_adress TEXT NULL,
+consolidated_longitude FLOAT NULL,
+consolidated_latitude FLOAT NULL,
+geojson_coordinates VARCHAR(255) NULL,
+nbr_poc INT NULL,
+power_voltage FLOAT NULL,
+free TINYINT NULL,
+access_condition VARCHAR(255) NULL,
+is_booked TINYINT NULL,
+hourly_access VARCHAR(255) NULL,
+access_prm TEXT NULL,
+motor_cycle_station TINYINT NULL,
+launching_date VARCHAR(255) NULL,
+update_date VARCHAR(255) NULL,
+datagouv_organization_or_owner TEXT NULL);
+
+CREATE TABLE payment_type(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    prepaid_payment TINYINT NULL,
+    c_card_payment TINYINT NULL,
+    cash_payment TINYINT NULL
+    );
+
+CREATE TABLE plug_type(id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+plug_type_ef TINYINT NULL,
+plug_type_2 TINYINT NULL,
+plug_type_combo_ccs TINYINT NULL,
+plug_type_chadeLu TINYINT NULL,
+plug_type_autre TINYINT NULL
+)
+
+
