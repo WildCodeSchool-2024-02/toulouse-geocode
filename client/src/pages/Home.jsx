9c5a00f9513ebce48d9ed934b86e7de7f755acdc
superwann32 <142965165+superwann32@users.noreply.github.com>
2024-06-14 09:55:37 +0200

    fix: debug and install sass

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/images/logo.svg";
 import "./Home.scss";
 
 function Home() {
   return (
     <div className="boxHome">
-      <img className="logoCSS" src={logo} alt="Logo du site WEB" />
+      <img className="logoCSS" src="public/logo.svg" alt="Logo du site WEB" />
       <p className="textHome">Trouvez une borne de recharge sur votre trajet</p>
-      <Link to="/connect" className="buttonHome">
+      <Link to="/map" className="buttonHome">
         Commencer
       </Link>
     </div>
