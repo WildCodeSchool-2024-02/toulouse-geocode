fbc46965519ecded730fb39603223114bb949e6e
BrebG <brebelgael@protonmail.com>
2024-07-15 09:55:10 +0200

    fix: commit befor pull

---
+++ client/src/pages/AdminBackOffice.jsx
@@ -0,0 +1,12 @@
+function AdminBackOffice() {
+  return (
+    <>
+      <h1>Interface administrateur</h1>
+      <div>
+        <h2>Messages</h2>
+      </div>
+    </>
+  );
+}
+
+export default AdminBackOffice;
