2ca59fc3e2e3c255fca710ec7a6f2144a91a2fee
MSR3030 <mansour32011@gmail.com>
2024-07-16 15:43:21 +0200

    feat: add plug filter componant

--- client/src/pages/Plug.jsx
+++ client/src/pages/Plug.jsx
@@ -1,6 +1,5 @@
+import { Link } from "react-router-dom";
 import "./Connect.scss";
-import "./button.scss";
-import "./input.scss";
 import "./Plug.scss";
 import plugcombo from "../../public/plugcombo.svg";
 import plugchade from "../../public/plugchade.svg";
@@ -8,25 +7,30 @@ import plugtype2 from "../../public/plugtype2.svg";
 
 function Plug() {
   return (
-    <div>
-      <select className="select-plug">Type de prise</select>
-      <div className="box-green">
-        <img
-          className="plug-css"
-          src={plugcombo}
-          alt="Prise de rechargement type 2"
-        />
-        <img
-          className="plug-css"
-          src={plugchade}
-          alt="Prise de rechargement type 2"
-        />
-        <img
-          className="plug-css"
-          src={plugtype2}
-          alt="Prise de rechargement type 2"
-        />
-      </div>
+    <div className="azer">
+      <h2 className="descr" id="texte">
+        Selectionnez la prise correspondante a votre vehicule
+      </h2>
+      <fieldset className="box-green">
+        <div>
+          <img src={plugcombo} alt="Prise de rechargement combo" />
+          <input type="radio" id="combo" name="drone" value="huey" checked />
+          <label htmlFor="huey">Prise Combo</label>
+        </div>
+        <div>
+          <img src={plugchade} alt="Prise de rechargement chade" />
+          <input type="radio" id="chade" name="drone" value="huey" checked />
+          <label htmlFor="huey">Prise Chade</label>
+        </div>
+        <div>
+          <img src={plugtype2} alt="Prise de rechargement type 2" />
+          <input type="radio" id="type2" name="drone" value="huey" checked />
+          <label htmlFor="huey">Prise Type 2</label>
+        </div>
+      </fieldset>
+      <Link to="/map" className="button-home">
+        Seletionner
+      </Link>
     </div>
   );
 }
