61c580614b45991d8f74f0a9a63f41a7f30a346e
Modrum <tristanlaroye@hotmail.com>
2024-07-23 10:33:46 +0200

    feat: add hud menu for mobile device in map

--- client/src/pages/Reservation.jsx
+++ client/src/pages/Reservation.jsx
@@ -113,21 +113,4 @@ function Reservation() {
   );
 }
 
-// export default Reservation;
-
-// import { useLocation } from "react-router-dom";
-
-// function Reservation() {
-//   const location = useLocation();
-
-//   return (
-//     <>
-//       <p>{location?.state?.startingDate}</p>
-//       <p>{location?.state?.endingDate}</p>
-//       <p>{location?.state?.userId}</p>
-//       <p>{location?.state?.stationId}</p>
-//     </>
-//   );
-// }
-
 export default Reservation;
