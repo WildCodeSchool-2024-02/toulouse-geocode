796d79c0057d0c082568a469d72e9e51af841668
Modrum <tristanlaroye@hotmail.com>
2024-07-30 12:29:52 +0200

    fix: scss transform on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,14 +4,14 @@ import "./Home.scss";
 
 function Home() {
   return (
-    <div className="box-home">
-      <img className="logo-css" src={logo} alt="Logo du site WEB" />
-      <p className="text-home">
-        Trouvez une borne de recharge sur votre trajet
-      </p>
-      <Link to="/map" className="button-lg-olive-fullfilled">
-        Commencer
-      </Link>
+    <div className="box-home-container">
+      <div className="box-home">
+        <img className="logo" src={logo} alt="Logo du site WEB" />
+        <p className="text-home">Trouvez une borne de recharge sur votre trajet</p>
+        <Link to="/map" className="button-lg-olive-fullfilled">
+          Commencer
+        </Link>
+      </div>
     </div>
   );
 }
